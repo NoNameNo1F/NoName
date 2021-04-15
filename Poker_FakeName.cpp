8d94145d0dcdd775d3d62a9359081592418fc717
NoNameNo1F <82032762+NoNameNo1F@users.noreply.github.com>
2021-04-15 20:22:52 +0700

    Delete Poker_FakeName.cpp

--- Poker_FakeName.cpp
+++
@@ -1,7 +0,0 @@
-#include "Poker_FakeName.h"
-
-string generateFakeName(){
-    vector<string> store = {"Vinh", "Quan", "Quynh", "An", "Hoang", "Huy", "Chuong", "Loc", "Lan", "Thinh", "Nien", "Tuan", "Phuc", "Phong"};
-    Random rng = Random::instance();
-    return store[rng.next(store.size())];
-}
